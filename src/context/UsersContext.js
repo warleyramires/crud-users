187c4f59553199b59140a1809bd5ba4fe338e28c
Warley Ramires <warleyramiresgoncalves@gmail.com>
2025-04-30 02:34:24 -0300

    Estrutura View UserForm e UserList

---
+++ src/context/UsersContext.js
@@ -0,0 +1,2 @@
+import React from "react";
+import users from "../data/users";
--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,68 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
 
-export default props => {
+export default ({route, navigation}) => {
+
+    const [user, setUser]= useState (route.params ? route.params : {})
+
     return( 
-        <Text> User Form </Text>
+       <View style={styles.form}>
+        <Text>Name</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={text => setUser({...user, name: text})}
+            value={user.name}
+            placeholder="Insira seu nome"
+            />
+        
+        <Text>Email</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={text => setUser({...user, email: text})}
+            value={user.email}
+            placeholder="Insira seu email"
+            />
+           
+        <Text>Senha</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={text => setUser({...user, senha: text})}
+            value={user.senha}
+            placeholder="Insira sua senha(mínimo 8 caracteres)"
+            />
+
+        <Text>Cargo</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={text => setUser({...user, cargo: text})}
+            value={user.cargo}
+            placeholder="Insira seu cargo"
+        />
+
+        <Button 
+            title="Criar Usuário"
+            onPress={() => {
+                navigation.goBack()
+            }}
+        />
+           
+       </View>
+
+       
     )
 }
+
+const styles = StyleSheet.create({
+
+    form:{
+        padding: 12,
+    },
+    input:{
+        height: 40,
+        borderColor: '#000',
+        borderWidth: 1,
+        marginBottom: 10,
+    }
+
+})
