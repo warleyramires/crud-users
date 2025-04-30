aa210fe3b1a67037babad22dd5def36b58191ab5
Warley Ramires <warleyramiresgoncalves@gmail.com>
2025-04-30 20:33:11 -0300

    Adição tela de Splash

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { useCallback, useEffect, useState } from 'react';
+import { View, StatusBar } from "react-native";
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
 import UserList from "./views/UserList";
 import UserForm from "./views/UserForm";
 import { Button } from "react-native-elements";
@@ -9,10 +15,47 @@ import { Icon } from "react-native-elements";
 import { UsersProvider } from "./context/UsersContext";
 
 const Stack = createNativeStackNavigator();
-
+SplashScreen.preventAutoHideAsync();
 export default (props) => {
+
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+       
+        await Font.loadAsync(Entypo.font);
+      
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+   
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <UsersProvider>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <StatusBar style="auto" /> 
+      <UsersProvider>
+      
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="UserList"
@@ -47,6 +90,10 @@ export default (props) => {
         </Stack.Navigator>
       </NavigationContainer>
     </UsersProvider>
+      </View>
+
+
+    
   );
 };
 
