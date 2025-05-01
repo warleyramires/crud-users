9d665c43d2bde2b659ec7009b25bd579484ba177
Warley Ramires <warleyramiresgoncalves@gmail.com>
2025-05-01 00:35:25 -0300

    Validação dos campos

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,91 +1,146 @@
-import React, { useContext, useState } from 'react';
-import { Text, TextInput, StyleSheet, View } from 'react-native';
-import { Button } from 'react-native-elements';
-import UsersContext from '../context/UsersContext';
+import React, { useContext, useState } from "react";
+import { Text, TextInput, StyleSheet, View } from "react-native";
+import { Button } from "react-native-elements";
+import UsersContext from "../context/UsersContext";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-export default ({route, navigation}) => {
-    const [user, setUser]= useState (route.params ? route.params : {})
-    const {dispatch} = useContext(UsersContext)
+const validationSchema = yup.object().shape({
+  nome: yup.string().required("Nome é obrigatório"),
+  email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
+  senha: yup
+    .string()
+    .min(8, "A senha deve ter pelo menos 8 caracteres")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/,
+      "Sua senha deve conter letras e números"
+    )
+    .required("Senha é obrigatória"),
+  cargo: yup.string().required("Cargo é obrigatório"),
+});
 
-    return( 
-       <View style={styles.form}>
-        <Text style={styles.label}>Nome</Text>
-        <TextInput
+export default ({ route, navigation }) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+  const { dispatch } = useContext(UsersContext);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues: user,
+  });
+
+  const onSubmit = (data) => {
+    dispatch({
+      type: user.id ? "updateUser" : "createUser",
+      payload: data,
+    });
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.form}>
+      <Text style={styles.label}>Nome</Text>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
             style={styles.input}
-            onChangeText={text => setUser({...user, nome: text})}
-            value={user.nome}
+            onChangeText={onChange}
+            value={value}
             placeholder="Insira seu nome"
             placeholderTextColor="#fff"
-            />
-        
-        <Text style={styles.label}>Email</Text>
-        <TextInput
+          />
+        )}
+        name="nome"
+      />
+      {errors.nome && <Text style={styles.error}>{errors.nome.message}</Text>}
+
+      <Text style={styles.label}>Email</Text>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            inputMode="email"
             style={styles.input}
-            onChangeText={text => setUser({...user, email: text})}
-            value={user.email}
+            onChangeText={(text) => {
+              const emailLowerCase =
+                text.charAt(0).toLowerCase() + text.slice(1);
+              onChange(emailLowerCase);
+            }}
+            value={value}
             placeholder="Insira seu email"
             placeholderTextColor="#fff"
-            />
-           
-        <Text style={styles.label}>Senha</Text>
-        <TextInput
-            style={styles.input}
-            onChangeText={text => setUser({...user, senha: text})}
-            value={user.senha}
-            placeholder="Insira sua senha(mínimo 8 caracteres)"
-            placeholderTextColor="#fff"
-            />
+            type="email"
+          />
+        )}
+        name="email"
+      />
+      {errors.email && <Text style={styles.error}>{errors.email.message}</Text>}
 
-        <Text style={styles.label}>Cargo</Text>
-        <TextInput
+      <Text style={styles.label}>Senha</Text>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
             style={styles.input}
-            onChangeText={text => setUser({...user, cargo: text})}
-            value={user.cargo}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Insira sua senha (mínimo 8 caracteres)"
+            placeholderTextColor="#fff"
+            secureTextEntry={true}
+          />
+        )}
+        name="senha"
+      />
+      {errors.senha && <Text style={styles.error}>{errors.senha.message}</Text>}
+
+      <Text style={styles.label}>Cargo</Text>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            style={styles.input}
+            onChangeText={onChange}
+            value={value}
             placeholder="Insira seu cargo"
             placeholderTextColor="#fff"
-            
-        />
+          />
+        )}
+        name="cargo"
+      />
+      {errors.cargo && <Text style={styles.error}>{errors.cargo.message}</Text>}
 
-        <Button 
-            title="Salvar"
-            onPress={() => {
-                dispatch({
-                    type: user.id ? 'updateUser' : 'createUser',
-                    payload: user,
-                })
-                navigation.goBack()
-            }}
-           
-        />
-           
-       </View>
-
-       
-    )
-}
+      <Button title="Salvar" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-    form:{
-        flex: 1,
-        padding: 12,
-        backgroundColor: "#000000",
-        color: "#fff",
-    },
-
-    label:{
-        fontSize: 20,
-        color: "#fff",    
-    },
-    input:{
-        height: 50,
-        borderColor: '#fff',
-        color: '#fff',
-        borderWidth: 1,
-        marginBottom: 10,
-        borderRadius: 5,
-        paddingLeft: 10,
-
-    }
-
-})
+  form: {
+    flex: 1,
+    padding: 12,
+    backgroundColor: "#000000",
+    color: "#fff",
+  },
+  label: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  input: {
+    height: 50,
+    borderColor: "#fff",
+    color: "#fff",
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 5,
+    paddingLeft: 10,
+  },
+  error: {
+    color: "red",
+    fontSize: 14,
+  },
+});
